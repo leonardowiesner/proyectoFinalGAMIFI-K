ffad0cdd63b2574f7581943c65a4ea7064336278
nil11503 <nilorritpeinado@gmail.com>
2023-02-02 20:00:42 +0100

    s
    s

--- Angular/proyectoFinal/src/app/interfaces/profesores-data.interface.ts
+++ Angular/proyectoFinal/src/app/interfaces/profesores-data.interface.ts
@@ -1,11 +1,5 @@
 export interface TeachersData {
-    nick: string;
-    nombre: string;
-    apellidos: string;
     centro: string;
-    email: string;
-    password: string;
-    repassword: string;
 
   }
   
