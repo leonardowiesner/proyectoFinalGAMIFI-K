8cd3680edaaddf49e4c3989e5eaf688093f68983
nil11503 <nilorritpeinado@gmail.com>
2023-02-08 20:06:54 +0100

    Cambios Ligeros

--- Angular/proyectoFinal/src/app/paginas/pages-routing.module.ts
+++ Angular/proyectoFinal/src/app/paginas/pages-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
 import { MainPageComponent } from './main-page/main-page.component';
+import { StudentPageComponent } from './student-page/student-page.component';
 
 
 //TODO: Definir las ruta/s que correspondan
 const routes: Routes = [
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: 'main', component: MainPageComponent, canActivate: [AuthGuard] },  
+  { path: '', redirectTo: '/student', pathMatch: 'full' },
+  { path: 'student', component: StudentPageComponent, canActivate: [AuthGuard] },  
 ];
 
 @NgModule({
--- Angular/proyectoFinal/src/app/paginas/pages.module.ts
+++ Angular/proyectoFinal/src/app/paginas/pages.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { StudentPageComponent } from './student-page/student-page.component';
+import { TeacherPageComponent } from './teacher-page/teacher-page.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, StudentPageComponent, TeacherPageComponent],
   imports: [
     PagesRoutingModule,
   ]
