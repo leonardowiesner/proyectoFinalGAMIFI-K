866bf5d5204eaa18b1808311d823c0d14b6778b1
nil11503 <nilorritpeinado@gmail.com>
2023-02-13 20:25:01 +0100

    V0.1

--- Angular/proyectoFinal/src/app/paginas/pages.module.ts
+++ Angular/proyectoFinal/src/app/paginas/pages.module.ts
@@ -3,12 +3,12 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { StudentPageComponent } from './student-page/student-page.component';
 import { TeacherPageComponent } from './teacher-page/teacher-page.component';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [MainPageComponent, StudentPageComponent, TeacherPageComponent],
   imports: [
     PagesRoutingModule,
-    
+    MatFormFieldModule
   ]
 })
 export class PagesModule { }
