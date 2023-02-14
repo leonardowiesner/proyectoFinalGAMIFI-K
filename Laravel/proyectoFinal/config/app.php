0a41a4020a88f4863a69a41af40ff56d6ce3f8a1
Leonardo Wiesner <leonardowiesner11@gmail.com>
2023-02-14 19:59:42 +0100

    Deleting folder aditional Laravel

--- Laravel/proyectoFinal/config/app.php
+++ Laravel/proyectoFinal/config/app.php
@@ -121,7 +121,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'nK2hcQo6Bl1zJQI38cDuDIzZTWuv3f72HcKzYVVFQ4o'),
+    'key' => env('APP_KEY', 'SomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
