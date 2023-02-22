d5ee70d8dd60cd09df8022b21e0f8e7c9fe63980
nil11503 <nilorritpeinado@gmail.com>
2023-02-22 19:07:43 +0100

    a

--- Angular/proyectoFinal/src/app/services/student.service.ts
+++ Angular/proyectoFinal/src/app/services/student.service.ts
@@ -15,26 +15,15 @@ export class StudentService {
     private http: HttpClient
   ) { }
 
-  login(data: LoginData): Observable<LoginData> {
-
-    return this.http.get<LoginData>(this.fakeUrl).pipe(
-      filter((value: any) => {
-        let found = false;
-
-        for (let i = 0; i < value.length; i++) {
-          if (value[i].email == data.email && value[i].password == data.password) {
-            found = true;
-            this.user = {
-              email: value[i].email,
-              password: value[i].password,
-            };
-            break;
-          }
-        }
-
-        return found;
-      })
-    );
+  login(data: LoginData) {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    let options = { headers: headers };
+    console.log(data);
+   
+    return this.http.post('http://127.0.0.1:8000/api/login/student', data, options);
   }
 
   register(data: string) {
