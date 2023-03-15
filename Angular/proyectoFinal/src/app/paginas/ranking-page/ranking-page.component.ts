b6ef7e0ce0a58e53948ea0f64ab088c8e97e965e
nil11503 <74736351+nil11503@users.noreply.github.com>
2023-03-15 19:46:01 +0100

    a

--- Angular/proyectoFinal/src/app/paginas/ranking-page/ranking-page.component.ts
+++ Angular/proyectoFinal/src/app/paginas/ranking-page/ranking-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentData } from 'src/app/interfaces/alumnos-data.interface';
-
+import { TeachersData } from 'src/app/interfaces/profesores-data.interface';
+import { TeacherService } from 'src/app/services/teacher.service';
 import { Ranking, RankingAnalysis, RankingService, RankingSolo } from 'src/app/services/ranking.service';
 
 
@@ -14,15 +15,17 @@ export class RankingPageComponent implements OnInit {
   rankingSolo: RankingSolo[]=[];
   rankingId: number | null;
   rankingAnalises: RankingAnalysis[] = [];
+  teacher?:TeachersData;
 
-  constructor(private route: ActivatedRoute,private rankingService: RankingService) {
+  constructor(private route: ActivatedRoute,private rankingService: RankingService,private teacherService: TeacherService) {
     this.rankingId=0;
+    this.teacher=this.teacherService.teacher;
   }
   
   ngOnInit() {
 
      this.rankingId = Number(this.route.snapshot.paramMap.get('id'));
-   
+    
 
      this.rankingService.getRankingAnalysis(this.rankingId).subscribe(data => {
        this.rankingAnalises=data;
@@ -32,4 +35,7 @@ export class RankingPageComponent implements OnInit {
 
   }
 
+  eliminarRegistro(id:number){
+    
+  }
 }
