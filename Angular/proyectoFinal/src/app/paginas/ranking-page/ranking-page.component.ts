0dcd00f4b899c7e95b3b466dd41afce758e91444
YANH <76054703+whaitking@users.noreply.github.com>
2023-04-25 19:04:20 +0200

    swat of ranks

--- Angular/proyectoFinal/src/app/paginas/ranking-page/ranking-page.component.ts
+++ Angular/proyectoFinal/src/app/paginas/ranking-page/ranking-page.component.ts
@@ -12,13 +12,12 @@ import 'sweetalert2/src/sweetalert2.scss';
 import Swal from 'sweetalert2';
 import { v4 as uuidv4 } from 'uuid';
 
-@Component( {
+@Component({
   selector: 'app-ranking-page',
   templateUrl: './ranking-page.component.html',
-  styleUrls: [ './ranking-page.component.css' ]
-} )
-export class RankingPageComponent implements OnInit
-{
+  styleUrls: ['./ranking-page.component.css']
+})
+export class RankingPageComponent implements OnInit {
   rankingSolo: RankingSolo[] = [];
   rankingId: number;
   rankingName: String | null;
@@ -32,12 +31,11 @@ export class RankingPageComponent implements OnInit
   name_practica: string;
   tarea: Tarea;
   nuevaTarea: boolean = false;
-  selectedFiles: { [ practiceId: number ]: File } = {};
+  selectedFiles: { [practiceId: number]: File } = {};
   practicesDelivered: any[] = [];
 
 
-  constructor ( private route: ActivatedRoute, private rankingService: RankingService, private teacherService: TeacherService, private studentService: StudentService )
-  {
+  constructor(private route: ActivatedRoute, private rankingService: RankingService, private teacherService: TeacherService, private studentService: StudentService) {
     this.rankingId = 0;
     this.teacher = this.teacherService.teacher;
     this.student = this.studentService.student;
@@ -58,269 +56,253 @@ export class RankingPageComponent implements OnInit
 
 
 
-  ngOnInit ()
-  {
+  ngOnInit() {
 
-    this.rankingId = Number( this.route.snapshot.paramMap.get( 'id' ) );
-    this.rankingName = this.route.snapshot.paramMap.get( 'name' );
+    this.rankingId = Number(this.route.snapshot.paramMap.get('id'));
+    this.rankingName = this.route.snapshot.paramMap.get('name');
 
-    this.rankingService.getRankingAnalysis( this.rankingId ).subscribe( data =>
-    {
-      this.rankingAnalises = data.map( analysis =>
-      {
+    this.rankingService.getRankingAnalysis(this.rankingId).subscribe(data => {
+      this.rankingAnalises = data.map(analysis => {
         const imageUrls = {
-          emotional: this.getImageUrl( analysis.emotional ),
-          thinking: this.getImageUrl( analysis.thinking ),
-          responsability: this.getImageUrl( analysis.responsability ),
-          cooperation: this.getImageUrl( analysis.cooperation ),
-          initiative: this.getImageUrl( analysis.initiative ),
+          emotional: this.getImageUrl(analysis.emotional),
+          thinking: this.getImageUrl(analysis.thinking),
+          responsability: this.getImageUrl(analysis.responsability),
+          cooperation: this.getImageUrl(analysis.cooperation),
+          initiative: this.getImageUrl(analysis.initiative),
         };
 
         return { ...analysis, imageUrls };
-      } );
-    } );
+      });
+    });
 
-    this.route.queryParamMap.subscribe( params =>
-    {
-      const id = params.get( 'id' );
-      const rankingName = params.get( 'rankingName' );
+    this.route.queryParamMap.subscribe(params => {
+      const id = params.get('id');
+      const rankingName = params.get('rankingName');
       // ... código para utilizar los valores de id y rankingName en el componente
-    } );
+    });
 
 
 
 
-    this.rankingService.getPractices( this.studentService.student.id, this.rankingId )
-      .subscribe( response =>
-      {
-        console.log( "student" + this.studentService.student.id );
-        console.log( "ID_rank" + this.rankingId );
-        if ( response.error )
-        {
-          console.log( response.error );
+    this.rankingService.getPractices(this.studentService.student.id, this.rankingId)
+      .subscribe(response => {
+        console.log("student" + this.studentService.student.id);
+        console.log("ID_rank" + this.rankingId);
+        if (response.error) {
+          console.log(response.error);
           // Puedes manejar errores específicos aquí
-        } else if ( response.status === 1 )
-        {
-          console.log( response );
+        } else if (response.status === 1) {
+          console.log(response);
           this.practicas = response.data;
           // Asignar 'response.data' directamente a 'this.practicas'
-        } else
-        {
-          console.log( response.error );
+        } else {
+          console.log(response.error);
           //console.error('Error al obtener las tareas pendientes'+response.error);
         }
-        console.log( this.practicas );
-      } );
+        console.log(this.practicas);
+      });
 
 
-    if ( this.teacher )
-    {
-      console.log( this.rankingId );
+    if (this.teacher) {
+      console.log(this.rankingId);
 
     }
 
   }
 
-  getImageUrl ( points: number ): string
-  {
+  getImageUrl(points: number): string {
     let rank = 'Bronze';
-    if ( points >= 1000 )
-    {
+    if (points >= 1000) {
       rank = 'Plata';
     }
-    if ( points >= 2000 )
-    {
+    if (points >= 2000) {
       rank = 'Gold';
     }
-    if ( points >= 4000 )
-    {
+    if (points >= 4000) {
       rank = 'Platino';
     }
-    if ( points >= 7000 )
-    {
+    if (points >= 7000) {
       rank = 'Diamante';
     }
-    if ( points >= 10000 )
-    {
+    if (points >= 10000) {
       rank = 'Challenger';
     }
-    return `assets/images/${ rank }.png`;
+    return `assets/images/${rank}.png`;
   }
 
-  acceptStudent ( id_student: number, id_rank: number )
-  {
-    this.rankingService.acceptStudent( id_student, id_rank ).subscribe( ( response ) =>
-    {
-      console.log( response.data + "Antes" );
+  descriptionSoftSkills(points: number, images: string, name: string) {
+    Swal.fire({
+      title: name+ "<br>Puntos "+ points,
+      text: this.getDescription(name),
+      imageUrl: images,
+      imageWidth: 300,
+      imageHeight: 300,
+      imageAlt: 'Custom image',
+    })
+  }
+
+  getDescription(name: string) {
+    if (name === "Emocional") {
+      return "Las emociones son reacciones psicofisiológicas que representan modos de adaptación del individuo cuando percibe un objeto, persona, lugar, suceso o recuerdo importante."
+    }
+    if (name === "Pensamiento") {
+      return "En su sentido más común, los términos pensamiento y pensar se refieren a procesos cognitivos conscientes que pueden ocurrir independientemente de la estimulación sensorial. Sus formas más paradigmáticas son el juicio, el razonamiento, la formación de conceptos, la resolución de problemas y la deliberación."
+    }
+    if (name === "Responsabilidad") {
+      return "La responsabilidad es un valor que está en la conciencia de la persona que estudia la Ética sobre la base de la moral. Puesto en práctica, se establece la magnitud de dichas acciones y de cómo afrontarlas de la manera más positiva e integral para ayudar en un futuro."
+    }
+    if (name === "Cooperacion") {
+      return "La cooperación es el resultado de una estrategia aplicada al objetivo, desarrollado por grupos de personas o instituciones que comparten un mismo interés u objetivo. En este proceso generalmente se emplean métodos colaborativos y asociativos que facilitan la consecución de la meta común."
+    }
+    if (name === "Iniciativa") {
+      return "En psicología, la iniciativa es un rasgo de la personalidad que impulsa a un individuo a comenzar algo que ve como necesario, normalmente de cara al ámbito social en el que se mueve. Viene del latín initium, 'principio' y es el ímpetu o el primer paso hacia una acción."
+    }
+    return"";
+  }
+
+  acceptStudent(id_student: number, id_rank: number) {
+    this.rankingService.acceptStudent(id_student, id_rank).subscribe((response) => {
+      console.log(response.data + "Antes");
 
       this.practicesDelivered = response.data;
 
 
-      console.log( response.data + "Despues" );
+      console.log(response.data + "Despues");
       this.showPracticasComponent = true;
 
-    } );
-    this.rankingService.getRankingAnalysis( this.rankingId ).subscribe( data =>
-    {
+    });
+    this.rankingService.getRankingAnalysis(this.rankingId).subscribe(data => {
       this.rankingAnalises = data;
-    } );
+    });
   }
 
-  denegateStudent ( id_student: number, id_rank: number )
-  {
-    this.rankingService.denegateStudent( id_student, id_rank ).subscribe( ( response ) =>
-    {
-      console.log( response.data + "Antes" );
+  denegateStudent(id_student: number, id_rank: number) {
+    this.rankingService.denegateStudent(id_student, id_rank).subscribe((response) => {
+      console.log(response.data + "Antes");
 
       this.practicesDelivered = response.data;
 
 
-      console.log( response.data + "Despues" );
+      console.log(response.data + "Despues");
       this.showPracticasComponent = true;
 
-    } );
-    this.rankingService.getRankingAnalysis( this.rankingId ).subscribe( data =>
-    {
+    });
+    this.rankingService.getRankingAnalysis(this.rankingId).subscribe(data => {
       this.rankingAnalises = data;
-    } );
+    });
   }
 
-  verPracticas ( id_student: number )
-  {
-    this.rankingService.getPracticesDelivered( id_student, this.rankingId ).subscribe( ( response ) =>
-    {
-      console.log( response.data + "Antes" );
+  verPracticas(id_student: number) {
+    this.rankingService.getPracticesDelivered(id_student, this.rankingId).subscribe((response) => {
+      console.log(response.data + "Antes");
 
       this.practicesDelivered = response.data;
 
 
-      console.log( response.data + "Despues" );
+      console.log(response.data + "Despues");
       this.showPracticasComponent = true;
-    } );
+    });
   }
-  changeCodeRank ()
-  {
+  changeCodeRank() {
     const uuid = uuidv4();
 
-    this.rankingService.changeCodeRank( this.rankingId, uuid ).subscribe( response =>
-    {
-      if ( response.status === 1 )
-      {
-        Swal.fire( {
+    this.rankingService.changeCodeRank(this.rankingId, uuid).subscribe(response => {
+      if (response.status === 1) {
+        Swal.fire({
           icon: 'success',
           title: 'Código de ranking actualizado',
           text: 'El código del ranking se ha actualizado exitosamente.',
-        } );
-      } else
-      {
-        Swal.fire( {
+        });
+      } else {
+        Swal.fire({
           icon: 'error',
           title: 'Error al actualizar el código de ranking',
           text: 'Ocurrió un error al actualizar el código del ranking.',
-        } );
+        });
       }
-    }, error =>
-    {
-      Swal.fire( {
+    }, error => {
+      Swal.fire({
         icon: 'error',
         title: 'Error al actualizar el código de ranking',
         text: 'Ocurrió un error al actualizar el código del ranking.',
-      } );
-    } );
+      });
+    });
   }
 
-  onFileSelected ( event: Event, practiceId: number ): void
-  {
+  onFileSelected(event: Event, practiceId: number): void {
     const target = event.target as HTMLInputElement;
     const files = target.files;
 
-    if ( files && files.length > 0 )
-    {
-      const file = files[ 0 ];
-      this.selectedFiles[ practiceId ] = file;
+    if (files && files.length > 0) {
+      const file = files[0];
+      this.selectedFiles[practiceId] = file;
     }
   }
 
 
 
-  eliminarRegistro ( id_rank: number, id_student: number )
-  {
-    if ( confirm( '¿Estás seguro de que quieres eliminar este registro?' ) )
-    {
-      console.log( id_rank, id_student )
-      this.rankingService.deleteStudenRanking( id_rank, id_student ).subscribe( {
-        next: ( value: any ) =>
-        {
+  eliminarRegistro(id_rank: number, id_student: number) {
+    if (confirm('¿Estás seguro de que quieres eliminar este registro?')) {
+      console.log(id_rank, id_student)
+      this.rankingService.deleteStudenRanking(id_rank, id_student).subscribe({
+        next: (value: any) => {
           this.return = value
         }
-      } );
-      this.rankingService.getRankingAnalysis( id_rank ).subscribe( data =>
-      {
+      });
+      this.rankingService.getRankingAnalysis(id_rank).subscribe(data => {
         this.rankingAnalises = data;
-      } );
+      });
     }
   }
-  downloadPracticeFile ( id_student: number, id_practice: number ): void
-  {
-    this.rankingService.downloadPracticeFile( id_student, id_practice ).subscribe( ( file: Blob ) =>
-    {
-      saveAs( file, 'practice_file.pdf' );
-    } );
+  downloadPracticeFile(id_student: number, id_practice: number): void {
+    this.rankingService.downloadPracticeFile(id_student, id_practice).subscribe((file: Blob) => {
+      saveAs(file, 'practice_file.pdf');
+    });
   }
-  uploadFile ( practiceId: number ): void
-  {
-    if ( this.selectedFiles[ practiceId ] )
-    {
-      const fileToUpload = this.selectedFiles[ practiceId ];
-      this.rankingService.uploadPracticeFile( this.studentService.student.id, practiceId, fileToUpload )
-        .subscribe( response =>
-        {
-          if ( !response.message )
-          {
-            Swal.fire( {
+  uploadFile(practiceId: number): void {
+    if (this.selectedFiles[practiceId]) {
+      const fileToUpload = this.selectedFiles[practiceId];
+      this.rankingService.uploadPracticeFile(this.studentService.student.id, practiceId, fileToUpload)
+        .subscribe(response => {
+          if (!response.message) {
+            Swal.fire({
               icon: 'success',
               title: 'Bien...',
               text: 'Archivo subido correctamente.!',
-            } )
-          } else
-          {
-            alert( 'Error al subir el archivo.' );
+            })
+          } else {
+            alert('Error al subir el archivo.');
           }
-        }, error =>
-        {
-          alert( 'Error al subir el archivo.' );
-        } );
-    } else
-    {
-      Swal.fire( {
+        }, error => {
+          alert('Error al subir el archivo.');
+        });
+    } else {
+      Swal.fire({
         icon: 'warning',
         title: 'Cuidado...',
         text: 'Por favor, selecciona un archivo para subir.!',
-      } )
+      })
     }
   }
 
-  editarPuntos ( id_rank: number, id_student: number, point: number )
-  {
-    console.log( point )
-    this.rankingService.editarPuntos( id_student, id_rank, point ).subscribe(
+  editarPuntos(id_rank: number, id_student: number, point: number) {
+    console.log(point)
+    this.rankingService.editarPuntos(id_student, id_rank, point).subscribe(
 
     );
-    this.rankingService.getRankingAnalysis( id_rank ).subscribe( data =>
-    {
+    this.rankingService.getRankingAnalysis(id_rank).subscribe(data => {
       this.rankingAnalises = data;
-    } );
+    });
 
   }
-  agregarTarea ()
-  {
-    console.log( this.rankingId );
+  agregarTarea() {
+    console.log(this.rankingId);
     this.nuevaTarea = false;
-    Swal.fire( {
+    Swal.fire({
       icon: 'success',
       title: 'Bien...',
       text: 'Tarea agregada al ranking !',
-    } )
-    this.rankingService.crearPractice( this.tarea.name, this.tarea.description, this.tarea.deadline_practice, this.rankingId ).subscribe();
+    })
+    this.rankingService.crearPractice(this.tarea.name, this.tarea.description, this.tarea.deadline_practice, this.rankingId).subscribe();
   }
 }
